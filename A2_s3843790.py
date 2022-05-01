b252958203576cc661ecbb1a777b5810a306323f
Jingxuan Feng <henryfeng24@pm.me>
2022-05-01 16:28:50 +1000

    Finished scene 5

--- A2_s3843790.py
+++ A2_s3843790.py
@@ -12,12 +12,13 @@ References in IEEE
 About story: This storyline uses characters and terminology from anime show "Kono Subarashii Sekai ni Shukufuku wo!".
 The storyline itself is original and based off the original anime.
 
-1. Kono Subarashii Sekai ni Shukufuku wo! Wiki (no date). Available at: https://konosuba.fandom.com/wiki/Kono_Subarashii_Sekai_ni_Shukufuku_wo!_Wiki (Accessed: 1 May 2022).
-2. Random Name Generator &mdash; Easy Random Name Picker (no date) Random Word Generator. Available at: https://randomwordgenerator.com/name.php (Accessed: 1 May 2022).
+[1]‘Kono Subarashii Sekai ni Shukufuku wo! Wiki’. https://konosuba.fandom.com/wiki/Kono_Subarashii_Sekai_ni_Shukufuku_wo!_Wiki (accessed May 01, 2022).
+[2]‘Online Ascii Art Creator’. https://www.ascii-art-generator.org/ (accessed May 01, 2022).
+[3]‘Random Name Generator &mdash; Easy Random Name Picker’, Random Word Generator. https://randomwordgenerator.com/name.php (accessed May 01, 2022).
+[4] Aceblade10, ‘All Megumin’s anime translated explosion quotes.’, r/Konosuba, Mar. 17, 2017. www.reddit.com/r/Konosuba/comments/5zz3gw/all_megumins_anime_translated_explosion_quotes/ (accessed May 01, 2022).
 """
 
 # Import modules
-import math
 import random
 
 # These are global variables used in multiple scenes, has pre-defined values if the user does not overwrite them
@@ -86,13 +87,15 @@ def end_game(message):
 
 # Functions for scenes
 
-# Prints the main menu / welcome text
+# Prints the main menu - ascii art generated using [3]
 def show_main_menu():
-    print(
-        "--------------------------------------------------------------- \n"
-        "Welcome to KonoSuba side-story, the text based spinoff game! \n"
-        "--------------------------------------------------------------- \n"
-    )
+    ascii_art = "    __ __      _   __     _____       ____                  _____ ________  ______   _______________  ______  __\n" \
+                "   / //_/___  / | / /___ / ___/__  __/ __ )____ _   _      / ___//  _/ __ \/ ____/  / ___/_  __/ __ \/ __ \ \/ /\n" \
+                "  / ,< / __ \/  |/ / __ \\__ \/ / / / __  / __ `/  (_)      \__ \ / // / / / __/     \__ \ / / / / / / /_/ /\  / \n" \
+                " / /| / /_/ / /|  / /_/ /__/ / /_/ / /_/ / /_/ /  _       ___/ // // /_/ / /___    ___/ // / / /_/ / _, _/ / /  \n" \
+                "/_/ |_\____/_/ |_/\____/____/\__,_/_____/\__,_/  (_)     /____/___/_____/_____/   /____//_/  \____/_/ |_| /_/   \n"
+
+    print(ascii_art + "\n")
 
 
 # Function for scene 1, ask user to enter their character's name, height and pick a weapon
@@ -141,6 +144,7 @@ def scene1():
 # Function for scene 2 - Work scene, user needs to select option 3 to progress the story
 def scene2():
     global user_char_name
+    # Introduces current scene and scenario to user
     print(
         "Hi " + user_char_name + " , it is almost the end of the day and a customer wants to negotiate price for a potion. \n"
                                  "You need to sell this as it is part of your task to help clear the store inventory. \n")
@@ -172,6 +176,7 @@ def scene2():
 
 # Function for scene 3 - Gives user a quest and will continue the story if they accept
 def scene3():
+    # Introduces current scene and scenario to user
     print("""
     As you go home, Luna, the Adventurer Guild receptionist offers you a quest - \n 
     Luna: "One of the demon lord's generals have deployed a small recon force near Axel..." 
@@ -219,13 +224,13 @@ def scene3():
 def user_attack_damage(user_weapon):
     damage = None
 
-    if(user_weapon == "CHUNCHUNMARU"):
+    if (user_weapon == "CHUNCHUNMARU"):
         damage = random.randint(2, 4)
-    elif(user_weapon == "KATANA"):
+    elif (user_weapon == "KATANA"):
         damage = random.randint(4, 6)
     elif (user_weapon == "LONG SWORD"):
         damage = random.randint(6, 10)
-    else: # only other possible value is RAPIER, thus don't have to use another else if
+    else:  # only other possible value is RAPIER, thus don't have to use another else if
         damage = random.randint(4, 8)
 
     return damage
@@ -236,7 +241,7 @@ def longbow_attack_damage():
     random_values = random.sample(range(1, 10), 3)
     miss = validate_userinput(random.randint(1, 10), random_values)
 
-    if(miss):
+    if (miss):
         longbow_damage = 0
     else:
         longbow_damage = random.randint(8, 15)
@@ -250,26 +255,27 @@ def critical_damage(blessing):
     critical_hit = False
 
     # Guaranteed critical hit with Aqua's blessing cast
-    if(blessing):
+    if (blessing):
         critical_hit = True
     else:
-       # chance of critical hit (chance of number being inside loop)
-       random_values = random.sample(range(1, 10), 4)
-       critical_hit = validate_userinput(random.randint(1, 10), random_values)
+        # chance of critical hit (chance of number being inside loop)
+        random_values = random.sample(range(1, 10), 4)
+        critical_hit = validate_userinput(random.randint(1, 10), random_values)
 
     return critical_hit
 
 
 # Function for scene 4 - Continuously fight enemy guard until his hp <= 0
 def scene4():
+    # Introduces current scene and scenario to user
     print("You get close to the camp of the recon force, and you notice a single guard guarding the only entry path. \n"
           "You're currently ahead of the party and will need to deal with the guard yourself. \n"
           "Aqua and Megumin have not yet caught up so their abilities will not be available here. \n")
 
     global user_char_weapon
     prompt_1 = "How will you proceed? \n" \
-             "1. Try sneak around the guard \n" \
-             "2. Prepare to attack"
+               "1. Try sneak around the guard \n" \
+               "2. Prepare to attack"
 
     attack = False
     # Continuously prompt for input until user selects option 2
@@ -288,22 +294,21 @@ def scene4():
 
     # Continuously prompt until the user kills the guard
     enemy_hp = 25
+    prompt_2 = "1. Attack with your weapon: " + user_char_weapon + " \n" \
+                "2. Tell Kazuma longbow attack; has chance of missing \n" \
+                "3. Tell Darkness to attack \n"
 
     while enemy_hp > 0:
-        prompt_2 = "The guard has " + str(enemy_hp) + " hp remaining. \n" \
-                    "1. Attack with your weapon: " + user_char_weapon + " \n" \
-                    "2. Tell Kazuma longbow attack; has chance of missing \n" \
-                    "3. Tell Darkness to attack \n"
 
-        print(prompt_2)
+        print("The guard has " + str(enemy_hp) + " hp remaining." + "\n" + prompt_2)
         user_choice = get_user_input_int()
         if (validate_userinput_int(user_choice, 3)):  # is the number of options offered to user
             if (user_choice == 1):
                 damage = user_attack_damage(user_char_weapon)
-                critical_hit = critical_damage(False) # False since Aqua cannot cast Blessing in this scene
+                critical_hit = critical_damage(False)  # False since Aqua cannot cast Blessing in this scene
 
                 # Deal 2x damage in critical hit or with damage buff
-                if(critical_hit):
+                if (critical_hit):
                     print("You've landed a critical hit!!!")
                     damage = damage * 2
 
@@ -321,17 +326,124 @@ def scene4():
                 print(result)
 
             elif (user_choice == 3):
-                enemy_hp = enemy_hp - 0
                 print("Darkness missed and deals 0 damage!")
         else:
             print("You did not enter a valid option! \n")
-
     # END OF scene4 FUNCTION
 
 
-# TODO: Function for scene 5
+# Utility function for scene 5 - adds more immersion to story
+# doesn't do much other than print one the few different ways Megumin casts her explosion spell, referenced [4]
+def megumin_explosion_chant():
+    chant1 = "Megumin: \"Darkness blacker than black and darker than dark, I beseech thee, combine with my deep crimson.\" \n" \
+             "Megumin: \"The time of awakening cometh. Justice, fallen upon the infallible boundary, appear now as an intangible distortion!\" \n" \
+             "Megumin: \"Dance, Dance, Dance! I desire for my torrent of power a destructive force: a destructive force without equal!\" \n" \
+             "Megumin: \"Return all creation to cinders, and come from the abyss!\" \n" \
+             "Megumin: \"EXPLOSION!!!\" \n"
+
+    chant2 = "Megumin: \"By my efflux of deep crimson, topple this white world!\" \n" \
+             "Megumin: \"EXPLOSION!!!\" \n"
+
+    chant3 = "Megumin: \"Detonation... Detonation... Detonation... \" \n" \
+             "Megumin: \"Wielder of the most glorious, powerful, and grand explosion magic, My name is Megumin\" \n" \
+             "Megumin: \"The blow that I am given to strike turns a blind eye to the fate of my kindred, rendering all hope of rebirth and anguish, and the model by which all forces are judged!\" \n" \
+             "Megumin: \"Pitiful creature... Synchronize yourself with the red smoke, and atone in a surge of blood!\" \n" \
+             "Megumin: \"EXPLOSION!!!\" \n"
+
+    chant4 = "Megumin: \"The tower of rebellion creeps upon man's world, \" \n" \
+             "Megumin: \"The unspoken faith displayed before me, The time has come!\" \n" \
+             "Megumin: \"Now, awaken from your slumber, and by my madness, be wrought!\" \n" \
+             "Megumin: \"EXPLOSION!!!\" \n"
+
+    list_of_chants = [chant1, chant2, chant3, chant4]
+    return random.choice(list_of_chants)
+
+
+# Function for scene 5 - Prompts user to fight multiple enemies
 def scene5():
-    return None
+    global daily_explosion_spell
+    global user_char_weapon
+
+    # Introduces current scene and scenario to user
+    print("You've successfully taken out the guard and move on with the rest of the part... \n"
+          "Your earlier attacks have caused quite a commotion, the scouts inside the camp is ready to face you! \n")
+
+    prompt = "1. Attack with your weapon: " + user_char_weapon + " \n" \
+             "2. Ask Aqua to cast \"Damage Buff\", increased damage in next attack \n" \
+             "3. Have Aqua cast \"Blessing\", increase chance for critical hit \n" \
+             "4. Tell Kazuma longbow attack; has chance of missing \n" \
+             "5. Ask Megumin to cast explosion (Warning: can only be used once per day!) \n" \
+             "6. Tell Darkness to attack \n"
+
+    # Variables used for battling multiple enemies
+    number_of_enemies = random.randint(3, 5)  # user could face anywhere from 3-5 enemies
+    number_of_enemies_defeated = 0 # Keeps track on number of enemies killed
+    damage_buff = False
+    blessing = False
+
+    # continue loop as long as user has not killed all enemies
+    while (number_of_enemies_defeated != number_of_enemies):
+        enemy_hp = random.randint(15, 30) # this time enemy hp varies
+        enemy_remaining = number_of_enemies - number_of_enemies_defeated
+        print("There are " + str(enemy_remaining) + " enemies left... \n")
+
+        while enemy_hp > 0:
+            print("The guard has " + str(enemy_hp) + " hp remaining." + "\n" + prompt)
+            user_choice = get_user_input_int()
+            if (validate_userinput_int(user_choice, 6)):  # is the number of options offered to user
+                if (user_choice == 1):
+                    damage = user_attack_damage(user_char_weapon)
+                    critical_hit = critical_damage(blessing)  # Deal 2x damage in critical hit or with damage buff
+
+                    # Small preventive step to stop meta gaming, critical hit and damage buff together will give 3x instead of 4x damage
+                    if (critical_hit and damage_buff):
+                        damage = damage * 3
+                        print("You've landed a critical hit with damage buff!!!")
+                    else:
+                        if (critical_hit or damage_buff):
+                            damage = damage * 2
+                            print("You've dealt extra damage!")
+
+                    enemy_hp = enemy_hp - damage
+                    print("Your attack deals " + str(damage) + " damage")
+
+                    # Reset buffs, only last 1 turn
+                    damage_buff = False
+                    blessing = False
+
+                elif (user_choice == 2):
+                    damage_buff = True
+                    print("Aqua: \"Damage Buff!\" \n")
+
+                elif (user_choice == 3):
+                    blessing = True
+                    print("Aqua: \"Blessing!\" \n")
+
+                elif (user_choice == 4):
+                    damage = longbow_attack_damage()
+                    enemy_hp = enemy_hp - damage
+                    result = ""
+                    if (damage == 0):
+                        result = "Kazuma has missed! Dealing 0 damage"
+                    else:
+                        result = "Kazuma dealt " + str(damage) + " damage"
+                    print(result)
+
+                elif (user_choice == 5):
+                    if(daily_explosion_spell):
+                        daily_explosion_spell = False
+                        enemy_hp = 0
+                        chant = megumin_explosion_chant()
+                        print(chant)
+                    else:
+                        print("Megumin has already casted her explosion spell today and exhausted her mana... \n")
+
+                elif (user_choice == 6):
+                    print("Darkness missed and deals 0 damage! \n")
+
+            else:
+                print("You did not enter a valid option! \n")
+        number_of_enemies_defeated = number_of_enemies_defeated + 1
 
 
 # TODO: Function for scene 6
@@ -348,7 +460,7 @@ def main():
     # scene1()
     # scene2()
     # scene3()
-    scene4()
+    # scene4()
     scene5()
     scene6()
 
